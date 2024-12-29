21a613c7d55b9a8655bcf2e39a25040f347bdefc
abubakar83 <abubakar.abs83@gmail.com>
2024-12-28 17:56:30 -0800

    logic set for buttons

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ const calculator = () => {
     let buttons = documents.querySelectorAll(".btn");
     let clear = docoments.querySelector('.clear');
     let equal = documents.querySelector('.equal');
+
+    buttons.forEach((botton) => {
+        buttons.addEventListener("click", (e)=>{
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
 }

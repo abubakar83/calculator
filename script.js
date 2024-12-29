8fd66d23ce87e7f81658c418f747e3ce07e3757b
abubakar83 <abubakar.abs83@gmail.com>
2024-12-28 17:50:07 -0800

    select css classes

---
+++ script.js
@@ -0,0 +1,6 @@
+const calculator = () => {
+    let screen = document.querySelector(".screen");
+    let buttons = documents.querySelectorAll(".btn");
+    let clear = docoments.querySelector('.clear');
+    let equal = documents.querySelector('.equal');
+}
